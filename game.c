126121150417313bb543fc6b989585705828dd58
saveen-official <saveengimsara@gmail.com>
2026-06-17 16:51:49 +0530

    Added 30 random interior walls

--- game.c
+++ game.c
@@ -3,6 +3,8 @@
 #include <time.h>
 
 int main() {
+	srand(time(NULL));
+
 	char map[15][15];
 	int hiddenTraps[15][15];
 
@@ -19,6 +21,19 @@ int main() {
 			hiddenTraps[i][j] = 0;
 		}
 	}
+
+	int wallsPlaced = 0;
+
+        while (wallsPlaced < 30) {
+                int randomRow = (rand() % 13) + 1;
+                int randomCol = (rand() % 13) + 1;
+
+                if (map[randomRow][randomCol] == ' ') {
+                        map[randomRow][randomCol] = '#';
+                        wallsPlaced++;
+                }
+        }
+
 	
 	for (int i = 0; i < 15; i++) {
 		for (int j = 0; j < 15; j++) {
