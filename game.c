804b4fc4b029b6b27516d2a5215b5926c3e62a73
saveen-official <saveengimsara@gmail.com>
2026-06-18 06:42:58 +0530

    Added comments to the code

--- game.c
+++ game.c
@@ -8,6 +8,7 @@ int main() {
 	char map[15][15];
 	int hiddenTraps[15][15];
 
+	// Setup exterior walls, interior empty spaces and Traps
 	for (int i = 0; i < 15; i++) {
 		for (int j = 0; j < 15; j++) {
 
@@ -22,6 +23,7 @@ int main() {
 		}
 	}
 
+	// Placing 30 random interior walls
 	int wallsPlaced = 0;
 
         while (wallsPlaced < 30) {
@@ -34,7 +36,7 @@ int main() {
                 }
         }
 
-	
+	// Printing the full map
 	for (int i = 0; i < 15; i++) {
 		for (int j = 0; j < 15; j++) {
 			printf("%c", map[i][j]);
